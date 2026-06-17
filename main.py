de518268204c6452deb279f864289eb4749d4062
jrvla <vladimir.artemj@gmail.com>
2026-06-17 23:14:49 +0500

    add: ppo

--- main.py
+++ main.py
@@ -2,22 +2,76 @@ import gymnasium as gym
 import ale_py
 from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo, AtariPreprocessing
 
-# import torch
-# import torch.nn as nn
+import numpy as np
+import os
+
+from collections import deque
+
+import torch
+import torch.nn as nn
+
+frameskips = 3
+model_path = f"{os.getcwd()}/models/ppo/pacman-agent.pth"
+
+class PacmanAgent(nn.Module):
+    def __init__(self, n_hid, n_out):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(frameskips, 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),         
+               
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+        )
+        
+        self.head = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 7 * 7, n_hid),
+            nn.ReLU(),  
+            nn.Linear(n_hid, n_out)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return self.head(x)
+
+def choose_action(model, state, epsilon):
+    if np.random.rand() < epsilon:
+        return np.random.randint(0, action_size)
+       
+    state = torch.tensor(state, dtype=torch.float32)
+    state = state.unsqueeze(0)
+    with torch.no_grad():
+        out = model(state)
+    return int(torch.argmax(out).item())
 
 gym.register_envs(ale_py)
 
-env = gym.make("ALE/DoubleDunk-v5", render_mode="human", frameskip=1)
-env = AtariPreprocessing(env)
+env = gym.make("ALE/Pacman-v5", render_mode="human", frameskip=1)
+env = AtariPreprocessing(env, frame_skip=frameskips, scale_obs=True)
 
 obs, info = env.reset()
 
 done = False
 
+action_size = env.action_space.n
+
+agent = PacmanAgent(512, action_size)
+agent.load_state_dict(torch.load(model_path))
+stack = deque(maxlen=frameskips)
+
+for _ in range(frameskips):
+    stack.append(obs)
+
 while not done:
-    action = env.action_space.sample()
+    action = choose_action(agent, stack, 0)
     
     obs, reward, terminated, truncated, info = env.step(action)
+    
+    stack.append(obs)
     
     done = terminated or truncated
     
---
+++ ppo.py
@@ -0,0 +1,106 @@
+import cv2
+import gymnasium as gym
+import ale_py
+from gymnasium.wrappers import RecordVideo, AtariPreprocessing, FrameStackObservation
+
+import numpy as np
+import os
+
+import torch
+import torch.nn as nn
+
+frameskips = 4
+num_recordings = 3
+model_path = f"{os.getcwd()}/models/ppo/pacman-agent.pth"
+video_dir = f"{os.getcwd()}/data/ppo/eval"
+
+class ResizeRender(gym.Wrapper):
+    def render(self):
+        frame = self.env.render()
+
+        return cv2.resize(
+            frame,
+            (640, 840),
+            interpolation=cv2.INTER_NEAREST
+        )
+
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    return layer
+
+class PacmanAgent(nn.Module):
+    def __init__(self, n_hid, n_out):
+        super().__init__()
+        self.network = nn.Sequential(
+            layer_init(nn.Conv2d(frameskips, 32, kernel_size=8, stride=4)),
+            nn.ReLU(),
+            
+            layer_init(nn.Conv2d(32, 64, kernel_size=4, stride=2)),
+            nn.ReLU(),         
+               
+            layer_init(nn.Conv2d(64, 64, kernel_size=3, stride=1)),
+            nn.ReLU(),
+            
+            nn.Flatten(),
+            layer_init(nn.Linear(64 * 7 * 7, n_hid)),
+            nn.ReLU(),  
+        )
+        
+        self.actor = layer_init(nn.Linear(n_hid, n_out), std=0.01)
+        self.critic = layer_init(nn.Linear(n_hid, 1), std=1)                
+
+    def get_value(self, x):
+        return self.critic(self.network(x))
+
+    def get_action(self, x):
+        hidden = self.network(x)
+        logits = self.actor(hidden)
+        
+        action = torch.argmax(logits, dim=1)
+        
+        return action
+
+    def get_action_and_value(self, x, action=None):
+        hidden = self.network(x)
+        logits = self.actor(hidden)
+        probs = torch.distributions.Categorical(logits=logits)
+        if action is None:
+            action = probs.sample()
+        return action, probs.log_prob(action), probs.entropy(), self.critic(hidden)
+
+gym.register_envs(ale_py)
+
+env = gym.make("ALE/Pacman-v5", render_mode="rgb_array", frameskip=1)
+env = AtariPreprocessing(env, frame_skip=frameskips, scale_obs=True)
+env = FrameStackObservation(env, stack_size=frameskips)
+env = ResizeRender(env)
+env = RecordVideo(env, video_folder=video_dir, episode_trigger=lambda x: True)
+
+action_size = env.action_space.n
+
+agent = PacmanAgent(512, action_size)
+agent.load_state_dict(torch.load(model_path))
+agent.eval()
+
+for recording in range(num_recordings):
+    total_reward = 0
+
+    obs, _ = env.reset()
+    
+    done = False
+    
+    while not done:
+            
+        obs_tensor = torch.tensor(obs, dtype=torch.float32).unsqueeze(0)
+        
+        with torch.no_grad():
+            action = agent.get_action(obs_tensor).item()
+        
+        obs, reward, terminated, truncated, info = env.step(action)
+            
+        total_reward += reward
+        
+        done = terminated or truncated    
+    print(total_reward)
+env.close()
